abe84235d1debc136328b732c60f5d480d08e938
Jaja Yting <jaltying@gmail.com>
2018-08-24 13:10:19 +0800

    Conformance to Equatable Protocol

--- Sources/TimePassed.swift
+++ Sources/TimePassed.swift
@@ -18,3 +18,36 @@ public enum TimePassed {
     case second(Int)
     case now
 }
+
+extension TimePassed: Equatable {
+    
+    public static func ==(lhs: TimePassed, rhs: TimePassed) -> Bool {
+        
+        switch(lhs, rhs) {
+            
+        case (.year(let a), .year(let b)):
+            return a == b
+            
+        case (.month(let a), .month(let b)):
+            return a == b
+            
+        case (.day(let a), .day(let b)):
+            return a == b
+            
+        case (.hour(let a), .hour(let b)):
+            return a == b
+            
+        case (.minute(let a), .minute(let b)):
+            return a == b
+            
+        case (.second(let a), .second(let b)):
+            return a == b
+            
+        case (.now, .now):
+            return true
+            
+        default:
+            return false
+        }
+    }
+}
